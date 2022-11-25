79f2c37d7e09f3c9d7c000720eefe9538ee52fda
mvpc-11232 <mvp2612@hotmail.com>
2022-11-25 08:24:10 +0530

    Auto Push Commit

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -2,7 +2,7 @@ def lintChecks() {
         sh "echo installing jslinst"
         sh "npm i jslint"   
         sh "node_modules/jslint/bin/jslint.js server.js || true"
-                sh "echo Lint Checks Completed"
+        sh "echo Lint Checks Completed"
 }
 
 
