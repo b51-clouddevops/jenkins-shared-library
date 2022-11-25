9eb38b3a2158b3820861b8ded45b810e2edd90d0
mvpc-11232 <mvp2612@hotmail.com>
2022-11-25 09:02:31 +0530

    Auto Push Commit

---
+++ vars/python.groovy
@@ -0,0 +1,24 @@
+def lintChecks() {
+    sh "echo "
+    sh "echo Lint Checks Completed for $COMPONENT"
+}
+
+
+// function call will be called by default, when you call the fileName
+def call() {
+    pipeline{
+        agent any 
+        stages {
+            stage('Lint Checks') {
+                steps {
+                    script {
+                        lintChecks()                  // Use script { when you're using groovy based conventions }
+                    }
+                }
+            }     
+        }   // end of stages 
+    }  // end of pipelines
+} // end of call
+
+
+
