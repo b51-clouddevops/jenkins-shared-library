c2398d18595e5d96bb34c1c137d10f36c5ce657c
mvpc-11232 <mvp2612@hotmail.com>
2022-11-25 08:49:16 +0530

    Auto Push Commit

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,7 +1,5 @@
 def lintChecks() {
-        sh "echo installing jslinst"
-        sh "npm i jslint"   
-        sh "node_modules/jslint/bin/jslint.js server.js || true"
+        sh "mvn checkstyle:check || true"
         sh "echo Lint Checks Completed for $COMPONENT"
 }
 
