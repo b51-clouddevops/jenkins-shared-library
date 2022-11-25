0658b8760c2fb216484d2769c543db80aca4cf32
mvpc-11232 <mvp2612@hotmail.com>
2022-11-25 08:22:54 +0530

    Auto Push Commit

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,18 +1,16 @@
-@Library('roboshop-shared-library@main') _
 
-pipeline{
-    agent any 
-    stages {
-        stage('Lint Checks') {
-            steps {
-                script {
-                    nodejs.lintChecks()                  // Use script { when you're using groovy based conventions }
+    pipeline{
+        agent any 
+        stages {
+            stage('Lint Checks') {
+                steps {
+                    script {
+                        nodejs.lintChecks()                  // Use script { when you're using groovy based conventions }
+                    }
                 }
-            }
-        } 
-    
-    }   // end of stages 
-}  // end of pipelines
+            }     
+        }   // end of stages 
+    }  // end of pipelines
 
 
 
