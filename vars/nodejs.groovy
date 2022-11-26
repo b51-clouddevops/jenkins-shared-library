53fe6029ce8c2de4bf169b91482abd2afd347702
mvpc-11232 <mvp2612@hotmail.com>
2022-11-26 08:12:13 +0530

    Auto Push Commit

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -8,7 +8,7 @@ def lintChecks() {
 def lintChecks() {
         sh "echo Starting Code Quality Analysis"
         sh "sonar-scanner -Dsonar.host.url=http://172.31.0.59:9000 -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=."
-
+}
 
 
 // function call will be called by default, when you call the fileName
@@ -22,7 +22,15 @@ def call() {
                         lintChecks()                  // Use script { when you're using groovy based conventions }
                     }
                 }
-            }     
+            }   
+
+            stage('Sonar Checks') {
+                steps {
+                    script {
+                        lintChecks()                  // Use script { when you're using groovy based conventions }
+                    }
+                }
+            } 
         }   // end of stages 
     }  // end of pipelines
 } // end of call
