d7c388d8e58e4f33d610159d63aa5f372b82a2ac
mvpc-11232 <mvp2612@hotmail.com>
2022-11-26 09:05:35 +0530

    asdf

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -8,7 +8,7 @@ def lintChecks() {
 def sonarChecks() {
         sh "echo Starting Code Quality Analysis"
         sh "sonar-scanner -Dsonar.host.url=http://172.31.0.59:9000 -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=."
-        sh "bash -x sonar-quality-gate.sh admin password ${SONARURL} cart"
+        sh "bash -x sonar-quality-gate.sh ${SONAR_USR} ${SONAR_PSW} ${SONARURL} ${COMPONENT}"
 }
 
 
