5dba66a9247914ef6464c2b83c006751bccd67fa
mvpc-11232 <mvp2612@hotmail.com>
2022-11-28 07:57:04 +0530

    Auto Push Commit

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,5 +1,5 @@
 def sonarChecks() {
         sh "echo Starting Code Quality Analysis"
-        sh "sonar-scanner -Dsonar.host.url=http://172.31.0.59:9000 -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=."
+        sh "sonar-scanner -Dsonar.host.url=http://172.31.0.59:9000 -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} -Dsonar.projectKey=${COMPONENT}"
         sh "bash -x sonar-quality-gate.sh ${SONAR_USR} ${SONAR_PSW} ${SONARURL} ${COMPONENT}"
 }
