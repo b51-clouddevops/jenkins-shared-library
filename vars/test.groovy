29d4e2b6bea8cb29917f0c68f1f134459159d0ac
mvpc-11232 <mvp2612@hotmail.com>
2022-11-30 09:09:28 +0530

    Auto Push Commit

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,5 +1,7 @@
 node {
-    stage('Example1')
+    stage('Example1') {
+        print 'Hello World'
+    }
     stage('Example2') {
         if (env.BRANCH_NAME == 'master') {
             echo 'I only execute on the master branch'
