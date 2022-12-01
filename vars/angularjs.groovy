efac9439ed9d1ba1883b27240a165844edf6900e
mvpc-11232 <mvp2612@hotmail.com>
2022-12-01 08:52:34 +0530

    Auto Push Commit

--- vars/angularjs.groovy
+++ vars/angularjs.groovy
@@ -13,3 +13,20 @@ def call() {
         }
     }
 }
+
+
+def call() {
+    node {
+        git branch: 'main', url: "https://github.com/b51-clouddevops/${COMPONENT}.git"
+        env.APPTYPE="maven"
+        common.lintChecks()
+        env.ARGS="-Dsonar.java.binaries=target/"
+        common.sonarChecks()   
+        common.testCases()
+        env.SONARURL = "172.31.0.59"
+        env.NEXUSURL = "172.31.2.247"
+        if(env.TAG_NAME != null ) {
+            common.artifacts()
+        }
+    }
+}
