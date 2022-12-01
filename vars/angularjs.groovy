4b98646ca48f3b92e51ac59e53297b3437580faa
mvpc-11232 <mvp2612@hotmail.com>
2022-12-01 08:30:58 +0530

    Auto Push Commit

---
+++ vars/angularjs.groovy
@@ -0,0 +1,14 @@
+def call() {
+    node {
+        env.APPTYPE="angularjs"
+        common.lintChecks()
+        env.ARGS="-Dsonar.java.binaries=target/"
+        common.sonarChecks()   
+        common.testCases()
+        env.SONARURL = "172.31.0.59"
+        env.NEXUSURL = "172.31.2.247"
+        if(env.TAG_NAME != null ) {
+            common.artifacts()
+        }
+    }
+}
