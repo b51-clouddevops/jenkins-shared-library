9beaa91ba86b00e039c745e5c9e1df23dea96edb
mvpc-11232 <mvp2612@hotmail.com>
2022-12-23 08:03:47 +0530

    Auto Push Commit

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -3,7 +3,7 @@ def call() {
 properties([
   parameters([
      choice(name: 'ENV', choices: ['dev', 'prod'],  description: 'Chose the env to deploy')
-     choice(name: 'ENV', choices: ['dev', 'prod'],  description: 'Chose the env to deploy')
+     choice(name: 'ACTION', choices: ['apply', 'destroy'],  description: 'Chose the action')
     
   ])
 ])
