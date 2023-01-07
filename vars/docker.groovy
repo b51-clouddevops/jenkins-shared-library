007762596bc990dec348cfa867160d995d4164b9
CodingManoj <mvp2612@hotmail.com>
2023-01-07 08:54:01 +0530

    Auto Push Commit

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -10,8 +10,8 @@ def call() {
         if(env.TAG_NAME != null ) {
             stage('Docker Push') {
             
-                 sh "docker tag 834725375088.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:latest 834725375088.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:"
-         
+                 sh "docker tag 834725375088.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:latest 834725375088.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME}"
+                 sh "aws ecr get-login-password --region us-east-1 |  -docker login --username AWS-password-stdin 834725375088.dkr.ecr.us-east-1.amazonaws.com"
             }
         }
     }
