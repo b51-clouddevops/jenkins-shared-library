f39233966b610ff24d4117dee798eb936e80cf48
CodingManoj <mvp2612@hotmail.com>
2023-01-07 09:03:08 +0530

    CodeAutoPush

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -11,7 +11,7 @@ def call() {
             stage('Docker Push') {
                 sh """
                    docker tag 834725375088.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:latest 834725375088.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME}
-                   aws ecr get-login-password --region us-east-1 | docker login --username AWS-password-stdin 834725375088.dkr.ecr.us-east-1.amazonaws.com
+                   aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 834725375088.dkr.ecr.us-east-1.amazonaws.com
                    docker push 834725375088.dkr.ecr.us-east-1.amazonaws.com/frontend:${TAG_NAME}
                    """
            }
